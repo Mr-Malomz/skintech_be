bf93a8aa9531a6ef9593f12dbfa05777bfaed166
demola malomo <malomo.alaba@yahoo.com>
2021-09-15 21:19:16 +0100

    images and collections model

--- models/types.go
+++ models/types.go
@@ -25,8 +25,29 @@ type User struct {
 	IsVerified     bool               `json:"isVerified,omitempty" bson:"isVerified,omitempty"`
 }
 
-type Uploads struct {
+type Gender struct {
+	MALE   string
+	FEMALE string
+}
+
+type Images struct {
+	Id          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	UserId      primitive.ObjectID `json:"_userId,omitempty" bson:"_userId,omitempty"`
+	Skin_diag   string             `json:"skin_diag,omitempty" bson:"skin_diag,omitempty"`
+	Loc_disease string             `json:"loc_disease,omitempty" bson:"loc_disease,omitempty"`
+	Dif_diag    string             `json:"dif_diag,omitempty" bson:"dif_diag,omitempty"`
+	Gender      Gender             `json:"gender,omitempty" bson:"gender,omitempty"`
+	Age         int                `json:"age,omitempty" bson:"age,omitempty"`
+	Country     string             `json:"country,omitempty" bson:"country,omitempty"`
+	Comp_desc   string             `json:"comp_desc,omitempty" bson:"comp_desc,omitempty"`
+	Fav_count   int                `json:"fav_count,omitempty" bson:"fav_count,omitempty"`
+	Img_url     string             `json:"img_url,omitempty" bson:"img_url,omitempty"`
+	Created_At  time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
 }
 
 type Collections struct {
+	Id         primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Img_Id     primitive.ObjectID `json:"_img_id,omitempty" bson:"_img_id,omitempty"`
+	User_Id    primitive.ObjectID `json:"_user_id,omitempty" bson:"_user_id,omitempty"`
+	Created_At time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
 }
