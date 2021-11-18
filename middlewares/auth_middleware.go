a17966ea8b626c2a28776f24cfeaa677389dd752
demola malomo <malomo.alaba@yahoo.com>
2021-11-18 11:01:40 +0100

    middleware error starter

--- middlewares/auth_middleware.go
+++ middlewares/auth_middleware.go
@@ -2,11 +2,9 @@ package middlewares
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strings"
 
-	"github.com/Mr-Malomz/skintech_be/config"
 	"github.com/Mr-Malomz/skintech_be/dtos"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
@@ -15,33 +13,33 @@ import (
 func AuthJWTMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		header := c.Request.Header.Get("Authorization")
-		splitHeader := strings.Split(header, "Bearer ")
-		reqToken := splitHeader[1]
 
-		if header == "" {
-			log.Fatal()
-			c.JSON(http.StatusForbidden, dtos.Response{Status: http.StatusInternalServerError, Message: "An authorization header is required"})
-			return
-		}
+		if header != "" {
+			splitHeader := strings.Split(header, " ")
+			if len(splitHeader) == 2 {
+				reqToken := splitHeader[1]
 
-		tkn, err := jwt.Parse(reqToken, func(t *jwt.Token) (interface{}, error) {
-			//validate the algorithm
-			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+				tkn, err := jwt.Parse(reqToken, func(t *jwt.Token) (interface{}, error) {
+					//validate the algorithm
+					if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+						return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+					}
+					return []byte("secret"), nil
+				})
+
+				if err != nil {
+					c.JSON(http.StatusForbidden, dtos.Response{Status: http.StatusForbidden, Message: "invalid authorization token"})
+					// c.AbortWithStatus(http.StatusForbidden)
+				}
+
+				if !tkn.Valid {
+					// c.Next()
+					c.JSON(http.StatusForbidden, dtos.Response{Status: http.StatusForbidden, Message: "invalid authorization token"})
+				} 
 			}
-			return []byte(config.EnvJWT()), nil
-		})
-
-		if err != nil {
-			log.Fatal()
-			c.JSON(http.StatusForbidden, dtos.Response{Status: http.StatusInternalServerError, Message: err.Error()})
-			return
-		}
-
-		if !tkn.Valid {
-			log.Fatal()
-			c.JSON(http.StatusForbidden, dtos.Response{Status: http.StatusInternalServerError, Message: "nvalid authorization token"})
-			return
+		} else {
+			c.JSON(http.StatusForbidden, dtos.Response{Status: http.StatusForbidden, Message: "An authorization header is required"})
+			c.AbortWithStatus(http.StatusForbidden)
 		}
 
 	}
